2e2d27f1653354ca6618349cb1f3ed7edcc82717
ZLinuxFan <zlinuxfan@gmail.com>
2016-07-22 11:36:54 +0300

    Added Unit Test First commit

---
+++ src/C#Code/FizzBuzz/FizzBuzz.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.UnitTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
