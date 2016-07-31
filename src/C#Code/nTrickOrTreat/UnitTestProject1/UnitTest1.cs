8b1d88926faf80065412f5065ee4e2b73d64e6e4
ZLinuxFan <ZLinuxFan@gmail.com>
2016-07-31 18:05:35 +0300

    Added first unit test

--- src/C#Code/nTrickOrTreat/UnitTestProject1/UnitTest1.cs
+++ src/C#Code/nTrickOrTreat/UnitTestProject1/UnitTest1.cs
@@ -4,8 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject1 {
     [TestClass]
     public class UnitTest1 {
+        //Vampires: 1, Zombies: 1, Witches: 1, Houses: 1
         [TestMethod]
-        public void TestMethod1() {
+        public void Should_return_4_for_1_1_1_1() {
+
         }
     }
 }
