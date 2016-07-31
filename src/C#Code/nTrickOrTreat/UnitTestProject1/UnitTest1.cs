9d94512133ef3c5494d231af9e21ec0cad6ccfe2
ZLinuxFan <ZLinuxFan@gmail.com>
2016-07-31 23:04:50 +0300

    Added new test for 1 1 1 and 1 house

--- src/C#Code/nTrickOrTreat/UnitTestProject1/UnitTest1.cs
+++ src/C#Code/nTrickOrTreat/UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1 {
@@ -7,7 +8,23 @@ namespace UnitTestProject1 {
         //Vampires: 1, Zombies: 1, Witches: 1, Houses: 1
         [TestMethod]
         public void Should_return_4_for_1_1_1_1() {
+            var trickOrTreat = new TrickOrTreat();
 
+            trickOrTreat.DivideTheTreats(1, 1, 1, 1).Should().Be(4);
+        }
+    }
+
+    internal class TrickOrTreat {
+        public TrickOrTreat() {
+        }
+
+        public int DivideTheTreats(int vamp, int zombies, int witches, int houses) {
+            const int candiesForVamp = 3;
+            const int candiesForZombies = 4;
+            const int candiesForWitches = 5;
+
+            int allChildren = vamp + zombies + witches;
+            return (vamp*candiesForVamp + zombies*candiesForZombies + witches*candiesForWitches) / allChildren;
         }
     }
 }
