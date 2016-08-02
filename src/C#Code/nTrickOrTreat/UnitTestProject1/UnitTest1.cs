6f63b0786d7a8cbec482d31bd94fe07b96669eb4
ZLinuxFan <ZLinuxFan@gmail.com>
2016-08-02 09:48:36 +0300

    Edded Unit tests and solution

--- src/C#Code/nTrickOrTreat/UnitTestProject1/UnitTest1.cs
+++ src/C#Code/nTrickOrTreat/UnitTestProject1/UnitTest1.cs
@@ -1,30 +1,45 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nTrickOrTreat;
 
 namespace UnitTestProject1 {
     [TestClass]
     public class UnitTest1 {
-        //Vampires: 1, Zombies: 1, Witches: 1, Houses: 1
+        //Vampires: 1, Zombies: 1, Witches: 1, Houses: 0
+        [TestMethod]
+        public void Should_return_0_for_1_1_1_0() {
+            var trickOrTreat = new TrickOrTreat();
+            var str = "Vampires: 1, Zombies: 1, Witches: 1, Houses: 0";
+            trickOrTreat.DivideTheTreats(str).Should().Be(0);
+        }
         [TestMethod]
         public void Should_return_4_for_1_1_1_1() {
             var trickOrTreat = new TrickOrTreat();
+            var str = "Vampires: 1, Zombies: 1, Witches: 1, Houses: 1";
 
-            trickOrTreat.DivideTheTreats(1, 1, 1, 1).Should().Be(4);
+            trickOrTreat.DivideTheTreats(str).Should().Be(4);
         }
-    }
+        [TestMethod]
+        public void Should_return_4_for_1_0_1_1() {
+            var trickOrTreat = new TrickOrTreat();
+            var str = "Vampires: 1, Zombies: 0, Witches: 1, Houses: 1";
 
-    internal class TrickOrTreat {
-        public TrickOrTreat() {
+            trickOrTreat.DivideTheTreats(str).Should().Be(4);
+        }
+        [TestMethod]
+        public void Should_return_4_for_1_2_3_1() {
+            var trickOrTreat = new TrickOrTreat();
+            var str = "Vampires: 1, Zombies: 2, Witches: 3, Houses: 1";
+
+            trickOrTreat.DivideTheTreats(str).Should().Be(4);
+        }
+        [TestMethod]
+        public void Should_return_36_for_3_2_1_10() {
+            var trickOrTreat = new TrickOrTreat();
+            var str = "Vampires: 3, Zombies: 2, Witches: 1, Houses: 10";
+
+            trickOrTreat.DivideTheTreats(str).Should().Be(36);
         }
 
-        public int DivideTheTreats(int vamp, int zombies, int witches, int houses) {
-            const int candiesForVamp = 3;
-            const int candiesForZombies = 4;
-            const int candiesForWitches = 5;
-
-            int allChildren = vamp + zombies + witches;
-            return (vamp*candiesForVamp + zombies*candiesForZombies + witches*candiesForWitches) / allChildren;
-        }
     }
 }
--- src/C#Code/nTrickOrTreat/nTrickOrTreat/Program.cs
+++ src/C#Code/nTrickOrTreat/nTrickOrTreat/Program.cs
@@ -1,12 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace nTrickOrTreat {
-    class Program {
+    internal enum Candies {
+        Vamp = 3,
+        Zombies = 4,
+        Whitches = 5
+    }
+
+    internal class Program {
         static void Main(string[] args) {
+            var trickOrTreat = new TrickOrTreat();
+            var fileName = "D:/CodeEval/src/C#Code/nTrickOrTreat/forTest.txt";
+            string[] datas;
+
+            ReadFile(fileName, out datas);
+
+            foreach (var data in datas) {
+                if (data != null)
+                {
+                    Console.WriteLine(trickOrTreat.DivideTheTreats(data));
+                }
+            }
+        }
+
+        private static void ReadFile(string fileName, out string[] datas) {
+            int index = 0;
+            datas = new string[40];
+
+            using (var reader = File.OpenText(fileName))
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    datas[index++] = line;
+                    if (null == line)
+                        continue;
+                }
+        }
+    }
+
+    public class TrickOrTreat {
+        public TrickOrTreat() {
+        }
+
+        public int DivideTheTreats(string str) {
+            var data = ParseData(str);
+
+            int vamp = data[0];
+            int zombies = data[1];
+            int witches = data[2];
+            var houses = data[3];
+            var allChildren = vamp + zombies + witches;
+
+            Debug.WriteLine("allChildren: " + allChildren);
+
+            var answer = (vamp*(int) Candies.Vamp + zombies*(int) Candies.Zombies + witches*(int) Candies.Whitches)*
+                         houses/allChildren;
+            Debug.WriteLine("answer" + answer);
+            return answer;
+        }
+
+        private int[] ParseData(string str) {
+            int[] data = new int[4];
+
+            var regex = new Regex(@"\d+");
+            var match = regex.Match(str);
+            int index = 0;
+
+            while (match.Success) {
+                data[index++] = Int32.Parse(match.ToString());
+                match = match.NextMatch();
+            }
+            
+            return data;
         }
     }
 }
